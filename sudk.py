ebe3795a82ac010dcba4ac9fe925e5e03a91f280
Fb185 <fredbobonejr@gmail.com>
2022-12-29 20:00:57 +0000

    added sudk + test.py is complete

--- sudk.py
+++ sudk.py
@@ -1,4 +1,85 @@
-def main():
+def main(puzzle):
+    csp = create_sudoku_csp(puzzle)
+    if AC3(csp):
+        print("Solution found:")
+        for i in range(len(puzzle)):
+            for j in range(len(puzzle[i])):
+                var = f"V{i},{j}"
+                print(next(iter(csp.domains[var])), end=" ")
+            print()
+    else:
+        print("No solution found.")
+    print(csp.count)
+
+def create_sudoku_csp(puzzle):
+
+    n = len(puzzle)
+
+    variables = [f"V{i},{j}" for i in range(n) for j in range(n)]
+    domains = {var: list(range(1, n + 1)) for var in variables}
+    neighbors = {var: set() for var in variables}
+
+    # Set domains for variables that have a value in the puzzle
+    for i, row in enumerate(puzzle):
+        for j, value in enumerate(row):
+            if value == 0:                 ##################OLHA PO DO ARMANDO
+                continue
+            var = f"V{i},{j}"
+            domains[var] = [value]
+
+    # Set neighbors for each variable
+    for i in range(n):
+        for j in range(n):
+            var = f"V{i},{j}"
+            # Add variables in the same row and column as neighbors
+            for k in range(n):
+                if k != j:
+                    neighbors[var].add(f"V{i},{k}")
+                if k != i:
+                    neighbors[var].add(f"V{k},{j}")
+            # Add variables in the same 3x3 grid as neighbors
+            i_start, j_start = 3 * (i // 3), 3 * (j // 3)
+            for di in range(3):
+                for dj in range(3):
+                    if (i_start + di, j_start + dj) != (i, j):
+                        neighbors[var].add(f"V{i_start+ di},{j_start +dj}")
+    return CSP(variables, domains, neighbors, puzzle)
+
+class CSP:
+    def __init__(self, variables, domains, neighbors, puzzle):
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.puzzle = puzzle
+        self.count = 0
+
+def AC3(csp, arcs=None):
+    if arcs is None:
+        arcs = [(Xi, Xj) for Xi in csp.variables for Xj in csp.neighbors[Xi]]
+    count1=0
+    while arcs:
+        Xi, Xj = arcs.pop()
+        if revise(csp, Xi, Xj):
+            #print(count1)
+            if len(csp.domains[Xi]) == 0:
+                return False
+            for Xk in csp.neighbors[Xi]:
+                if Xk != Xj:
+                    arcs.append((Xk, Xi))
+    return True
+
+def revise(csp, Xi, Xj): 
+    revised = False
+    if(len(csp.domains[Xi]) > 1 and len(csp.domains[Xj]) >1):
+        return False
+    for x in csp.domains[Xi][:]:
+        csp.count  += 1
+        if not any( x!=y for y in csp.domains[Xj]):
+            csp.domains[Xi].remove(x)
+            revised = True
+    return revised
+
+if __name__ == "__main__":
     '''puzzle=[
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -10,6 +91,7 @@ def main():
         [0, 0, 0, 4, 1, 9, 0, 0, 5],
         [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]'''
+
     puzzle = [
         [0, 0, 3, 0, 2, 0, 6, 0, 0],
         [9, 0, 0, 3, 0, 5, 0, 0, 1],
@@ -21,91 +103,6 @@ def main():
         [8, 0, 0, 2, 0, 3, 0, 0, 9],
         [0, 0, 5, 0, 1, 0, 3, 0, 0]
         ]
-    csp = create_sudoku_csp(puzzle)
-    csp.curr_domains = {var: csp.domains[var].copy() for var in csp.variables}
-    print("Given Sudoku:")
-    
-    for i in range(len(puzzle)):
-        txt =""
-        for j in range(len(puzzle[i])):
-            txt +=  str(puzzle[i][j]) + " " 
-        print(txt)
-    
-    if AC3(csp):
-        print("Solution found:")
-        for i in range(len(puzzle)):
-            for j in range(len(puzzle[i])):
-                var = f"V{i},{j}"
-                print(next(iter(csp.curr_domains[var])), end=" ")
-            print()
-    else:
-        print("No solution found.")
+    main(puzzle)
 
-def create_sudoku_csp(puzzle):
-    n = len(puzzle)
-    variables = [f"V{i},{j}" for i in range(n) for j in range(n)]
-    domains = {var: set(range(1, n+1)) for var in variables}
-    neighbors = {var: set() for var in variables}
-    for i, row in enumerate(puzzle):
-        for j, value in enumerate(row):
-            if value == 0:
-                continue
-            var = f"V{i},{j}"
-            domains[var] = {value}
-    for i in range(n):
-        for j in range(n):
-            var = f"V{i},{j}"
-            for k in range(n):
-                if k != j:
-                    neighbors[var].add(f"V{i},{k}")
-                if k != i:
-                    neighbors[var].add(f"V{k},{j}")
-            i_start, j_start = 3 * (i // 3), 3 * (j // 3)
-            for di in range(3):
-                for dj in range(3):
-                    if (i_start+di, j_start+dj) != (i, j):
-                        neighbors[var].add(f"V{i_start+di},{j_start+dj}")
-    return CSP(variables, domains, neighbors, puzzle)
 
-class CSP:
-    def __init__(self, variables, domains, neighbors, puzzle):
-        self.variables = variables
-        self.domains = domains
-        self.neighbors = neighbors
-        self.puzzle = puzzle
-        self.curr_domains = None
-
-    def update_curr_domains(self):
-        self.curr_domains = {var: set(self.domains[var]) for var in self.variables}
-
-    def constraints(self, Xi, x, Xj, y):
-        i, j = map(int, Xi[1:].split(","))
-        if self.puzzle[i][j] != 0:
-            return self.puzzle[i][j] == x
-        if x == y:
-            return False
-        return True
-
-def AC3(csp, arcs=None):
-    if arcs is None:
-        arcs = [(Xi, Xk) for Xi in csp.variables for Xk in csp.neighbors[Xi]]
-    while arcs:
-        Xi, Xj = arcs.pop()
-        if revise(csp, Xi, Xj):
-            if not csp.curr_domains[Xi]:
-                return False
-            for Xk in csp.neighbors[Xi]:
-                if Xk != Xj:
-                    arcs.append((Xk, Xi))
-    return True
-
-def revise(csp, Xi, Xj):
-    revised = False
-    for x in csp.curr_domains[Xi].copy():
-        if not any(csp.constraints(Xi, x, Xj, y) for y in csp.curr_domains[Xj]):
-            csp.curr_domains[Xi].remove(x)
-            revised = True
-    return revised
-
-if __name__ == "__main__":
-    main()
--- test.py
+++ test.py
@@ -5,161 +5,118 @@ c = 1
 global b
 def train(X, validation_data):
 
-  print("train")
-  global p
-  global dictionary
-  global n
-  # Read documents X and labels Y
-  documents = []
-  labels = []
-  with open(X, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-      documents.append(row[1])
-      labels.append(row[0])
+    print("train")
+    global p
+    global dictionary
+    global n
+    # Read documents X and labels Y
+    documents = []
+    labels = []
+    with open(X, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            documents.append(row[1])
+            labels.append(row[0])
 
   # Compute dictionary D of X with n words
-  dictionary = set()
-  for document in documents:
-    words = document.split()
-    for word in words:
-      dictionary.add(word)
-  n = len(dictionary)
+    dictionary = set()
+    for document in documents:
+        words = document.split()
+        for word in words:
+            dictionary.add(word)
+    n = len(dictionary)
 
   # Compute m, mham, and mspam
-  m = len(documents)
-  mham = labels.count('ham')
-  print(mham)
-  mspam = labels.count('spam')
-  print(mspam)
+    m = len(documents)
+    mham = labels.count('ham')
+    mspam = labels.count('spam')
 
   # Initialize b
-  b = math.log(c) + math.log(mham) - math.log(mspam)
+    b = math.log(c) + math.log(mham) - math.log(mspam)
 
   # Initialize p with pij = i, wspam = n, wham = n
-  p = [[0] * n for _ in range(2)]
-  wspam = n
-  wham = n
+    p = [[0] * n for _ in range(2)]
+    wspam = n
+    wham = n
 
   # Count occurrence of each word
-  for i in range(m):
-    if labels[i] == 'spam':
-      for j in range(n):
-        word = list(dictionary)[j]
-        p[0][j] += documents[i].count(word)
-        wspam += documents[i].count(word)
-    else:
-      for j in range(n):
-        word = list(dictionary)[j]
-        p[1][j] += documents[i].count(word)
-        wham += documents[i].count(word)
-    print("wspam = ", wspam)
-    print("wham = ", wham)
+    for i in range(m):
+        if labels[i] == 'spam':
+            for j in range(n):
+                word = list(dictionary)[j]
+                p[0][j] += documents[i].count(word)
+                wspam += documents[i].count(word)
+                print("found in spam: ", word)
+                print("spam email n ", i)
+                print("wspam so far: ", wspam)
+        else:
+            for j in range(n):
+                word = list(dictionary)[j]
+                p[1][j] += documents[i].count(word)
+                wham += documents[i].count(word)
+                print("found in ham: ", word)
+                print("ham email n ", i)
+                print("wham so far: ", wspam)
 
 
   # Normalize counts to yield word probabilities
-  for j in range(n):
-    p[0][j] = (p[0][j] + 1) / (wspam + 2)  # Add a small positive value to p[0][j] to prevent math domain error
-    p[1][j] = (p[1][j] + 1) / (wham + 2)  # Add a small positive value to p[1][j] to prevent math domain error
-
-
-  validation_documents, validation_labels = validation_data
-  correct = 0
-  for i in range(len(validation_labels)):
-    score_threshold = 0
     for j in range(n):
-      word = list(dictionary)[j]
-      score_threshold += validation_documents[i].count(word) * (math.log(p[0][j]) - math.log(p[1][j]))
+        p[0][j] = (p[0][j]+1) / (wspam + 2)  # Add a small positive value to p[0][j] to prevent math domain error
+        p[1][j] = (p[1][j]+1) / (wham + 2)  # Add a small positive value to p[1][j] to prevent math domain error
 
-    if score_threshold > 0 and validation_labels[i] == 'spam':
-      correct += 1
-    elif score_threshold <= 0 and validation_labels[i] == 'ham':
-      correct += 1
+    print("total words of spam: ", wspam)
+    print("total words of ham: ", wham)
+    return b, p
 
-  precision = correct / len(validation_labels)
 
-  return p, precision
+def classify(x, b, p):
+  # Initialize score threshold t = -b
+    t = -b
 
-def classify(X, p):
+  # Loop through words in dictionary
+    for j in range(n):
+        t += x.count(list(dictionary)[j]) * (math.log(p[0][j]+1) - math.log(p[1][j]+1))
+
+  # Classify document based on score threshold
+    if t > 0:
+        return 'spam'
+    else:
+        return 'ham'
+
+def evaluate(test_file, b, p):
   # Read test data from CSV file
-  documents = []
-  labels = []
-  with open(X, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-      documents.append(row[1])
-      labels.append(row[0])
+    test_documents = []
+    test_labels = []
+    with open(test_file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            test_documents.append(row[1])
+            test_labels.append(row[0])
 
-  # Compute dictionary D of X with n words
-  dictionary = set()
-  for document in documents:
-    words = document.split()
-    for word in words:
-      dictionary.add(word)
-  n = len(dictionary)
+  # Classify test documents and compare to true labels
+    correct = 0
+    for i in range(len(test_documents)):
+        prediction = classify(test_documents[i], b, p)
+        if prediction == test_labels[i]:
+            correct += 1
 
-  # Classify each document in the test set
-  correct = 0
-  for i in range(len(documents)):
-    score_threshold = 0
-    for j in range(n):
-      word = list(dictionary)[j]
-      score_threshold += documents[i].count(word) * (math.log(p[0][j]) - math.log(p[1][j]))
-
-    if score_threshold > 0 and labels[i] == 'spam':
-      correct += 1
-    elif score_threshold <= 0 and labels[i] == 'ham':
-      correct += 1
-
-  precision = correct / len(labels)
-  return precision
+  # Calculate and return precision
+    precision = correct / len(test_labels)
+    return precision
 
 
-p, precision = train("./data/shortdataset.csv", "./data/validationSet.csv")
-print(f'Precision on validation set: {precision:.2f}')
-X_test = "./data/testSet.csv"
-precision_test = classify(X_test, p)
-print(f'Precision on test set: {precision_test:.2f}')
-#b = train("./data/shortdataset.csv", "./data/validationSet.csv")
-#precision = classify("./data/testSet.csv", b)
-#print(f'Precision: {precision:.9f}')
+X = './data/shortdataset.csv'
+# X = './data/spamHamDataset.csv'
 
+# Train classifier
+b, p = train(X, './data/validationSet.csv')
 
-#def classify(test_file, b):
-#  # Read test data from CSV file
-#  test_documents = []
-#  test_labels = []
-#  with open(test_file, 'r') as csv_file:
-#    csv_reader = csv.reader(csv_file)
-#    for row in csv_reader:
-#      test_documents.append(row[1])
-#      test_labels.append(row[0])
-#
-#  # Classify test documents
-#  predictions = []
-#  for x in test_documents:
-#    score_threshold = -b
-#    for j in range(n):
-#      word = list(dictionary)[j]
-#      score_threshold += x.count(word) * (math.log(p[0][j]) - math.log(p[1][j]))
-#
-#    if score_threshold > 0:
-#      predictions.append('spam')
-#    else:
-#      predictions.append('ham')
-#
-#  # Calculate precision
-#  correct = 0
-#  for i in range(len(test_labels)):
-#    if test_labels[i] == predictions[i]:
-#      correct += 1
-#  precision = correct / len(test_labels)
-#
-#  return precision
-#
-#
-#
-#b = train("./data/shortdataset.csv")
-#precision = classify("./data/testSet.csv", b)
-#print(f'Precision: {precision:.9f}')
-#
+# Test classifier on test dataset
+test_file = './data/testSet.csv'
+precision = evaluate(test_file, b, p)
+print(f'Test precision: {precision:.9f}')
+
+# Test classifier on validation dataset
+validation_file = './data/validationSet.csv'
+precision = evaluate(validation_file, b, p)
+print(f'Validation precision: {precision:.9f}')
